b179a2688eeee5dfbeb60e367377a65bdbd19437
Harsh Patel <harsh@prakashinfotech.com>
2025-06-03 13:02:30 +0530

    added bootstrap

--- README.md
+++ README.md
@@ -29,3 +29,11 @@ React Folder Structure
 (e). layouts
 
 - layout name implies that it's contain layouts component such as sidebar,topbar,footer,main
+
+Now we are adding Bootstrap in react so to add bootstrap in react follow bellow command.
+
+    1. Inside terminal copy paste the commond to install bootstrap using npm - npm i bootstrap
+    2. Here we are using scss to modify the bootstrap default class so inside assests folder there is css folder and in that create scss file 
+    3. Import all required module for scss or just copy from offical docs (https://getbootstrap.com/docs/5.3/customize/sass/#importing)
+    4. Do neccessary change and compiled the scss file and link css file to app.js/index.js 
+    5. Include bootstrap budle in index.js file and you are ready to go
--- src/layout/topbar/Topbar.js
+++ src/layout/topbar/Topbar.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
 const Topbar = () => {
-  return (
-    <div>Topbar</div>
-  )
+    return (
+        <div className='topbar'>
+            <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Offcanvas navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="#">Dashboard</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Link</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Topbar
